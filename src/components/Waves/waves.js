c9aa3f098556d2fcb91d95d0ad7a15b8495a6e17
Vijay Karthik Bethapudi <vijaykarthik.b@1digitals.com>
2022-11-10 15:47:01 +0530

    Add color palette

--- src/components/Waves/waves.js
+++ src/components/Waves/waves.js
@@ -58,10 +58,10 @@ const Waves = () =>{
                     <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
                     </defs>
                     <g class="parallax">
-                    <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgb(203, 218, 213,0.7" />
-                    <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgb(203, 218, 213,0.5)" />
-                    <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgb(203, 218, 213,0.3)" />
-                    <use xlinkHref="#gentle-wave" x="48" y="7" fill="#0f1012" />
+                    <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(216, 215, 196, 0.7)" />
+                    <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgb(216, 215, 196,0.5)" />
+                    <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgb(216, 215, 196,0.3)" />
+                    <use xlinkHref="#gentle-wave" x="48" y="7" fill="#000000" />
                     </g>
                     </svg>
                 </div>
