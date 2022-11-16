3e88bc2db5868024cfc6792328bb326dbd11dc50
Vijay Karthik Bethapudi <vijaykarthik.b@1digitals.com>
2022-11-16 16:34:06 +0530

    Add event listeners for smallNav

--- src/components/SmallNav/smallNav.js
+++ src/components/SmallNav/smallNav.js
@@ -1,17 +1,47 @@
-import './smallNav.scss';
-import {IoHomeOutline} from 'react-icons/io5';
-import {BiUser, BiBookContent} from 'react-icons/bi';
-import { FiPhoneCall } from 'react-icons/fi'
+import "./smallNav.scss";
+import { IoHomeOutline } from "react-icons/io5";
+import { BiUser, BiBookContent } from "react-icons/bi";
+import { FiPhoneCall } from "react-icons/fi";
+import { useState } from "react";
 
-const SmallNav = () =>{
-    return (
-        <nav className='small-nav'>
-            <a href="#" aria-label="Home"><IoHomeOutline/></a>
-            <a href="#about" aria-label="About"><BiUser/></a>
-            <a href="#portfolio" aria-label="Portfolio"><BiBookContent/></a>
-            <a href="#contact" aria-label="Contact"><FiPhoneCall/></a>
-        </nav>
-    )
-}
+const SmallNav = () => {
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav className="small-nav">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+        aria-label="Home"
+      >
+        <IoHomeOutline />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+        aria-label="About"
+      >
+        <BiUser />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+        aria-label="Portfolio"
+      >
+        <BiBookContent />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+        aria-label="Contact"
+      >
+        <FiPhoneCall />
+      </a>
+    </nav>
+  );
+};
 
-export default SmallNav
+export default SmallNav;
