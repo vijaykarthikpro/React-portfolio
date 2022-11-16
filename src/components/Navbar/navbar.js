5837bd3a849a3d1b4c7d7695bec9d730593781f3
Vijay Karthik Bethapudi <vijaykarthik.b@1digitals.com>
2022-11-17 03:22:59 +0530

    Add about me and logo

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,21 +1,15 @@
 import { Link } from 'react-router-dom'
 import Sidebar from '../Sidebar/sidebar'
 import './navbar.scss'
-
-// import LogoS from'../../assets/images/logo-s.png'
+import logo from '../../assets/images/logo.png';
 
 const Navbar = () => {
     return (
         <div className='nav-bar'>
             <Link className='logo' to='/'>
-                <img src="" alt='logo'/>
+                <img src={logo} alt='logo'/>
+                {/* <span>VIJAY KARTHIK BETHAPUDI</span> */}
             </Link>
-            <nav>
-                <a href="#" className='btn'>Home</a>
-                <a href="#about" className='btn'>About</a>
-                <a href="#portfolio" className='btn'>Portfolio</a>
-                <a href="#contact" className='btn'>Contact</a>
-            </nav>
             <Sidebar/>
             <span className='scroll-down'>Scroll down -{'>'}</span>
         </div>
