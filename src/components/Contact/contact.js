f924e8f0b10c5088ca9645b94facc5c88663df9f
Vijay Karthik Bethapudi <vijaykarthik.b@1digitals.com>
2022-12-01 22:32:04 +0530

    Add contact section

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,8 +1,31 @@
 import './contact.scss';
+import React from 'react';
+import {HiOutlineMail} from 'react-icons/hi';
 
 const Contact = ()=>{
     return (
-        <section id='contact'>Contact section</section>
+        <section id='contact'>
+            <div className="container contact__container">
+                <div className="content flex-centered">
+                    <span>Get In Touch</span>
+                </div>
+                <div className='contact__options'>
+                    <article className='contact__option'>
+                        <HiOutlineMail className='contact__option-icon'/>
+                        <h4>Email</h4>
+                        <h5>vijaykarthikpro@gmail.com</h5>
+                        <a href='mailto:vijaykarthikpro@gmail.com' target='_blank'>Send a message</a>
+                    </article>
+                    <form action=''>
+                        <input type='text' name='name' placeholder='Your Full Name' required/>
+                        <input type='email' name='email' placeholder='Your Email' required/>
+                        <textarea name='message' rows='7' placeholder='Your Message' required/>
+                        <button type='submit' className='btn btn-primary'>Send Message</button>
+                    </form>
+                </div>
+                
+            </div>
+        </section>
     )
 }
 
