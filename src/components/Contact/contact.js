4b2866b24916a470a0267b5696d84bca89bd7042
Vijay Karthik Bethapudi <vijaykarthik.b@1digitals.com>
2022-12-01 22:55:20 +0530

    Add email service

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,8 +1,23 @@
 import './contact.scss';
-import React from 'react';
+import React, {useRef} from 'react';
+import emailjs from 'emailjs-com'
 import {HiOutlineMail} from 'react-icons/hi';
 
 const Contact = ()=>{
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_y6l0kef', 'template_g8f3yia', form.current, 'frf_J3ztbugv3cgKW')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+        
+          e.target.reset();
+    };
     return (
         <section id='contact'>
             <div className="container contact__container">
@@ -14,9 +29,9 @@ const Contact = ()=>{
                         <HiOutlineMail className='contact__option-icon'/>
                         <h4>Email</h4>
                         <h5>vijaykarthikpro@gmail.com</h5>
-                        <a href='mailto:vijaykarthikpro@gmail.com' target='_blank'>Send a message</a>
+                        <a href='mailto:vijaykarthikpro@gmail.com' rel="noreferrer" target='_blank'>Send a message</a>
                     </article>
-                    <form action=''>
+                    <form ref={form} onSubmit={sendEmail}>
                         <input type='text' name='name' placeholder='Your Full Name' required/>
                         <input type='email' name='email' placeholder='Your Email' required/>
                         <textarea name='message' rows='7' placeholder='Your Message' required/>
